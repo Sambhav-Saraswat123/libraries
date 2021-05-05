042408eb8f3e4b84a0e9f6f8ad3fe062a254f9e1
Sambhav-Saraswat123 <76476750+Sambhav-Saraswat123@users.noreply.github.com>
2021-05-05 17:49:06 +0530

    Add files via upload

---
+++ drawShape.js
@@ -0,0 +1,35 @@
+class Elipse {
+  constructor(xpoint, ypoint, radius, borderColor, color, lineWidth) {
+    this.xpoint = xpoint;
+    this.ypoint = ypoint;
+    this.radius = radius;
+    this.borderColor = borderColor;
+    this.color = color;
+    this.lineWidth = lineWidth;
+  }
+  drawCircle(ctx) {
+    ctx.beginPath();
+    ctx.arc(this.xpoint, this.ypoint, this.radius, 0, Math.PI * 2, false);
+    ctx.strokeStyle = this.borderColor;
+    ctx.lineWidth = this.lineWidth;
+    ctx.fillStyle = this.color;
+    ctx.fill();
+    ctx.closePath();
+    ctx.stroke();
+  }
+}
+class Rectangle {
+  constructor(xpoint, ypoint, width, height, fillStyle) {
+    this.xpoint = xpoint;
+    this.ypoint = ypoint;
+    this.width = width;
+    this.height = height;
+    this.fillStyle = fillStyle;
+  }
+  drawRect(ctx) {
+    ctx.beginPath();
+    ctx.rect(this.xpoint, this.ypoint, this.width, this.height);
+    ctx.fillStyle = this.fillStyle;
+    ctx.fill();
+  }
+}
